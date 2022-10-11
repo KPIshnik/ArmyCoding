65af2f6863aa6858ccebfe1899ec3fda629f6aca
KPIshnik <dimoglo.anton@gmail.com>
2022-10-11 12:48:57 +0300

    added some comments to regUser test

--- test/unit/controllers/registerUserr.test.js
+++ test/unit/controllers/registerUserr.test.js
@@ -1,17 +1,21 @@
 const registerUser = require("../../../controllers/registerUserr");
+
+// *******
+// Мокаю все импорты в registerUser
 jest.mock("../../../models/checkIsRegistered");
 jest.mock("../../../models/registerNewUser");
-jest.mock("../../../helpers/sendConfirmEmailHelper");
+jest.mock("../../../helpers/сonfirmEmailHelper");
 jest.mock("../../../models/checkUniqueUsername");
 
 const checkIsRegistered = require("../../../models/checkIsRegistered");
 const registerNewUser = require("../../../models/registerNewUser");
-const sendConfirmEmailHelper = require("../../../helpers/sendConfirmEmailHelper");
+const сonfirmEmailHelper = require("../../../helpers/сonfirmEmailHelper");
 const checkUniqueUsername = require("../../../models/checkUniqueUsername");
+//***** */
 
 describe("Register user controller test", () => {
-  //hashed pass??
-
+  // ********************
+  // мокаю Responce
   const mockResponse = {
     status: jest.fn(() => {
       return mockResponse;
@@ -21,22 +25,25 @@ describe("Register user controller test", () => {
     }),
     sendStatus: jest.fn(),
   };
+  //********************* */
 
+  //** */
   const mockNextFunction = jest.fn();
+  //** */
 
   beforeEach(() => {
-    mockResponse.status.mockClear();
-    mockResponse.json.mockClear();
-    sendConfirmEmailHelper.mockClear();
-    registerNewUser.mockClear();
-    checkIsRegistered.mockReset();
-    checkUniqueUsername.mockReset();
+    mockResponse.status.mockClear(); // сброс(очистка всех вызовов)
+    mockResponse.json.mockClear(); // сброс(очистка всех вызовов)
+    сonfirmEmailHelper.mockClear(); // сброс(очистка всех вызовов)
+    registerNewUser.mockClear(); // сброс(очистка всех вызовов)
+    checkIsRegistered.mockReset(); // сброс(очистка всех вызовов)
+    checkUniqueUsername.mockReset(); // сброс(очистка всех вызовов)
   });
 
   describe("testing with differen user data", () => {
     beforeEach(() => {
-      checkIsRegistered.mockResolvedValueOnce(false);
-      checkUniqueUsername.mockResolvedValueOnce(true);
+      checkIsRegistered.mockResolvedValueOnce(false); // Один раз выдать фалс
+      checkUniqueUsername.mockResolvedValueOnce(true); // Один раз выдать тру
     });
 
     test("should register useer with correct data", async () => {
@@ -48,30 +55,34 @@ describe("Register user controller test", () => {
         email: "bob@gmail.com",
       };
 
-      const mockRequest = { body: testUser };
+      const mockRequest = { body: testUser }; // мокаю запрос
 
       //act
       await registerUser(mockRequest, mockResponse, mockNextFunction);
 
       //assert
+      // *****
+      // проверяю что  респонс вызван с праильными аргументами
       expect(mockResponse.status).toHaveBeenCalledWith(200);
       expect(mockResponse.json).toHaveBeenCalledWith(
         `user ${testUser.userName} registered, please confirm email`
       );
 
-      expect(sendConfirmEmailHelper).toHaveBeenCalledTimes(1);
-      expect(registerNewUser).toHaveBeenCalledTimes(1);
+      // ***********
+      expect(сonfirmEmailHelper).toHaveBeenCalledTimes(1); // проверка на количесвто вызвовов
 
+      // проверяю что  registerNewUser вызван с праильными аргументами
       expect(registerNewUser).toHaveBeenCalledWith(
         null,
         testUser.userName,
-        expect.any(String),
+        expect.any(String), // тут хз надо ли проверять что пароль хэшнут и как это делать
         null,
         null,
         "email"
       );
 
-      expect(sendConfirmEmailHelper).toHaveBeenCalledWith(
+      // проверяю что  сonfirmEmailHelper вызван с праильными аргументами
+      expect(сonfirmEmailHelper).toHaveBeenCalledWith(
         testUser.userName,
         testUser.email
       );
@@ -96,8 +107,8 @@ describe("Register user controller test", () => {
         `pass too short or does not match`
       );
 
-      expect(sendConfirmEmailHelper).toHaveBeenCalledTimes(0);
-      expect(registerNewUser).toHaveBeenCalledTimes(0);
+      expect(сonfirmEmailHelper).toHaveBeenCalledTimes(0); // проверяю чтоб не был вызван
+      expect(registerNewUser).toHaveBeenCalledTimes(0); // проверяю чтоб не был вызван
     });
 
     test("should not register user with empty username", async () => {
@@ -117,7 +128,7 @@ describe("Register user controller test", () => {
       expect(mockResponse.status).toHaveBeenCalledWith(400);
       expect(mockResponse.json).toHaveBeenCalledWith(`username missing`);
 
-      expect(sendConfirmEmailHelper).toHaveBeenCalledTimes(0);
+      expect(сonfirmEmailHelper).toHaveBeenCalledTimes(0);
       expect(registerNewUser).toHaveBeenCalledTimes(0);
     });
 
@@ -138,15 +149,15 @@ describe("Register user controller test", () => {
       expect(mockResponse.status).toHaveBeenCalledWith(400);
       expect(mockResponse.json).toHaveBeenCalledWith(`email missing`);
 
-      expect(sendConfirmEmailHelper).toHaveBeenCalledTimes(0);
+      expect(сonfirmEmailHelper).toHaveBeenCalledTimes(0);
       expect(registerNewUser).toHaveBeenCalledTimes(0);
     });
   });
 
   test("should not register user with not unique email", async () => {
     //arrange
-    checkIsRegistered.mockReturnValueOnce(true);
-    checkUniqueUsername.mockReturnValueOnce(true);
+    checkIsRegistered.mockReturnValueOnce(true); // один раз вернуть тру
+    checkUniqueUsername.mockReturnValueOnce(true); // один раз вернуть тру
 
     const testUser = {
       userName: "Bob",
@@ -166,14 +177,14 @@ describe("Register user controller test", () => {
     );
 
     expect(checkUniqueUsername).toHaveBeenCalledTimes(0);
-    expect(sendConfirmEmailHelper).toHaveBeenCalledTimes(0);
+    expect(сonfirmEmailHelper).toHaveBeenCalledTimes(0);
     expect(registerNewUser).toHaveBeenCalledTimes(0);
   });
 
   test("should not register user with not unique usename", async () => {
     //arrange
-    checkIsRegistered.mockReturnValueOnce(false);
-    checkUniqueUsername.mockReturnValueOnce(false);
+    checkIsRegistered.mockReturnValueOnce(false); // один раз вернуть фалс
+    checkUniqueUsername.mockReturnValueOnce(false); // один раз вернуть фалс
 
     const testUser = {
       userName: "Bob",
@@ -191,7 +202,7 @@ describe("Register user controller test", () => {
     expect(mockResponse.json).toHaveBeenCalledWith(
       `this username already registered, try another one`
     );
-    expect(sendConfirmEmailHelper).toHaveBeenCalledTimes(0);
+    expect(сonfirmEmailHelper).toHaveBeenCalledTimes(0);
     expect(registerNewUser).toHaveBeenCalledTimes(0);
   });
 
