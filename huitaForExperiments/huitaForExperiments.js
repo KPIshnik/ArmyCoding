d675370e1f71bfd1f6941074c3a31678820228b5
KPIshnik <dimoglo.anton@gmail.com>
2023-03-23 17:58:30 +0200

    gitignore

--- huitaForExperiments/huitaForExperiments.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs").promises;
-const path = require("path");
-const pathToLog = path.join(__dirname, "../log/errlog.txt");
-
-fs.stat(pathToLog).then((data) => {
-  console.log(+data.mtime);
-  console.log(new Date());
-});
